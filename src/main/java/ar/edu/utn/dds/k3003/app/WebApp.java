c980706324d596e2040c4d4ab3ba256bc90ca739
Sabrina Garcia <mvlts9819@gmail.com>
2024-10-01 02:34:10 -0300

    implementando grafana

--- src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -10,6 +10,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.javalin.Javalin;
+import io.javalin.micrometer.MicrometerPlugin;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmHeapPressureMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.prometheusmetrics.PrometheusConfig;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -18,12 +27,14 @@ import java.util.TimeZone;
 public class WebApp {
 
     public static void main(String[] args) {
-
+        // crea un registro para nuestras métricas basadas en Prometheus
+        final var registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        String TOKEN = System.getenv("TOKEN_METRICS");
         // Iniciar el worker en un hilo separado
         iniciarWorkerSensorTemperaturas();
 
         // Iniciar la API
-        Javalin app = iniciarApiJavalin();
+        Javalin app = iniciarApiJavalin(registry);
 
         var objectMapper = createObjectMapper();
         var fachada = crearFachada(objectMapper);
@@ -39,6 +50,27 @@ public class WebApp {
         app.post("/depositos", heladeraController::depositar);
         app.post("/retiros", heladeraController::retirar);
         app.get("/cleanup", heladeraController::cleanup);
+
+        // agregamos una ruta para hacer polling de las métrivas con el
+        // handler que toma llos resultadose scrapear la registry de
+        // métricas
+        app.get("/metrics",
+                ctx -> {
+                    // chequear el header de authorization y chequear el token bearer
+                    // configurado
+                    var auth = ctx.header("Authorization");
+
+                    if (auth != null && auth.intern() == "Bearer " + TOKEN) {
+                        ctx.contentType("text/plain; version=0.0.4")
+                                .result(registry.scrape());
+                    } else {
+                        // si el token no es el apropiado, devolver error,
+                        // desautorizado
+                        // este paso es necesario para que Grafana online
+                        // permita el acceso
+                        ctx.status(401).json("unauthorized access");
+                    }
+                });
     }
 
     // Iniciar el worker de RabbitMQ
@@ -62,9 +94,42 @@ public class WebApp {
     }
 
     // Inicializar la API
-    private static Javalin iniciarApiJavalin() {
-        int port = Integer.parseInt(System.getProperty("port", "8080")); // Usar puerto 8080 por defecto
-        return Javalin.create().start(port);
+    private static Javalin iniciarApiJavalin(PrometheusMeterRegistry registry) {
+
+        System.out.println("starting up the server");
+
+
+        // agregar aquí cualquier tag que aplique a todas las métrivas de la app
+        // (e.g. EC2 region, stack, instance id, server group)
+        registry.config().commonTags("app", "metrics-sample");
+
+        // agregamos a nuestro reigstro de métricas todo lo relacionado a infra/tech
+        // de la instancia y JVM
+        try (var jvmGcMetrics = new JvmGcMetrics();
+             var jvmHeapPressureMetrics = new JvmHeapPressureMetrics()) {
+            jvmGcMetrics.bindTo(registry);
+            jvmHeapPressureMetrics.bindTo(registry);
+        }
+        new JvmMemoryMetrics().bindTo(registry);
+        new ProcessorMetrics().bindTo(registry);
+        new FileDescriptorMetrics().bindTo(registry);
+
+        // agregamos métricas custom de nuestro dominio
+        Gauge.builder("myapp_random", () -> (int)(Math.random() * 1000))
+                .description("Random number from My-Application.")
+                .strongReference(true)
+                .register(registry);
+
+        // seteamos el registro dentro de la config de Micrometer
+        final var micrometerPlugin =
+                new MicrometerPlugin(config -> config.registry = registry);
+
+        // registramos el plugin de Micrometer dentro de la config de la app de
+        // Javalin
+         return Javalin.create(config -> { config.registerPlugin(micrometerPlugin); })
+                .start(8080);
+
+
     }
 
     // Configurar el ObjectMapper
